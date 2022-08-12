4cf47991d30ea6bb5e4696383b67ed87684f8d9c
Ttatyanakr <Ttatyanakr@yandex.ru>
2022-08-12 06:35:51 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
+let minNum = 0;
+let maxNum = 0;
+let result;
+
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        minNum = min;
+        maxNum = max;
     }
 
     guess() {
-
+        result = Math.round((minNum + maxNum) / 2);
+        return result;
     }
 
     lower() {
-
+        maxNum = result;
     }
 
     greater() {
-
+        minNum = result;
     }
 }
 
